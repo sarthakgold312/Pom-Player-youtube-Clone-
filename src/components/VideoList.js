3a5f54f98b931fc86193f58099fcee72d1a27de1
Sarthak Singh <mzppsarthakjvb@gmail.com>
2024-07-31 05:32:12 +0530

    first commit

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,22 +1,3 @@
-// import React from 'react';
-// import VideoItem from './VideoItem';
-
-// const VideoList = ({videos , onVideoSelect}) => {
-//     const renderVideos = videos.map((videos) => {
-//         return(
-//             <VideoItem key={videos.id.videoId} video = {videos} onVideoSelect= {onVideoSelect} />
-//         );
-//     });
-//     return(
-//         <div>{renderVideos}</div>
-//     );
-// }
-
-
-// export default VideoList;
-
-
-
 import React from 'react';
 import VideoItem from './VideoItem';
 
